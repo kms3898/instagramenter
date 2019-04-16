446433fb0ca87d1c6ef11b41e785fef3e5c787ad
kms3898 <49633599+kms3898@users.noreply.github.com>
2019-04-16 18:45:01 +0900

    Add files via upload

--- changeenter.js
+++ changeenter.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
   function changeenter(){
     var frm = document.instatext;
     frm.text1.value = frm.text1.value.replace(/(?:\r\n|\r|\n)/g,'\u00a0\n');
@@ -7,4 +7,3 @@
     document.execCommand("copy");
     alert("변환 후 복사되었습니다 :)");
   }
-  </script>
